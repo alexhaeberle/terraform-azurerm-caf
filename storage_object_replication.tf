b5e66042d83e6df3306dd97bf44ede86ef11c1f7
Alex Haeberle <alex.haeberle@gmail.com>
2022-02-01 10:57:37 -0500

    udpated source dest storage ids to combined object

--- storage_object_replication.tf
+++ storage_object_replication.tf
@@ -7,8 +7,18 @@ module "storage_object_replication" {
   source = "./modules/storage_account/object_replication"
   for_each = var.object_replication
 
-  source_storage_account_id       = each.value.source_storage_account_id
-  destination_storage_account_id  = each.value.destination_storage_account_id
+  source_storage_account_id       = try(
+    # local.combined_objects_storage_accounts[each.value.storage_accounts.lz_key][each.value.storage_accounts.key].name,
+    local.combined_objects_storage_accounts[each.value.storage_accounts.lz_key][each.value.source_storage_account_key].id,
+    # local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.storage_accounts.key].name,
+    local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.source_storage_account_key].id
+  )
+  destination_storage_account_id  = try(
+    # local.combined_objects_storage_accounts[each.value.storage_accounts.lz_key][each.value.storage_accounts.key].name,
+    local.combined_objects_storage_accounts[each.value.storage_accounts.lz_key][each.value.destination_storage_account_key].id,
+    # local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.storage_accounts.key].name,
+    local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.destination_storage_account_key].id
+  )
   settings                        = each.value
 }
 
