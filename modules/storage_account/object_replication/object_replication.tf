a15497e6807720248ca4aa8299ff04a060ba7610
Alex Haeberle <alex.haeberle@gmail.com>
2022-02-01 11:03:37 -0500

    Added try to vars in obj rep

--- modules/storage_account/object_replication/object_replication.tf
+++ modules/storage_account/object_replication/object_replication.tf
@@ -8,8 +8,8 @@ resource "azurerm_storage_object_replication" "obj_repl" {
     content {
       source_container_name = rules.value.source_container_name
       destination_container_name = rules.value.destination_container_name
-      copy_blobs_created_after = try(rules.value.copy_blobs_created_after)
-      filter_out_blobs_with_prefix = try(rules.value.filter_out_blobs_with_prefix)
+      copy_blobs_created_after = try(rules.value.copy_blobs_created_after, null)
+      filter_out_blobs_with_prefix = try(rules.value.filter_out_blobs_with_prefix, null)
     }
   }
 
@@ -17,10 +17,10 @@ resource "azurerm_storage_object_replication" "obj_repl" {
     for_each = var.settings.timeouts
 
     content {
-      create = try(timeouts.value.create)
-      read = try(timeouts.value.read)
-      update = try(timeouts.value.update)
-      delete = try(timeouts.value.delete)
+      create = try(timeouts.value.create, null)
+      read = try(timeouts.value.read, null)
+      update = try(timeouts.value.update, null)
+      delete = try(timeouts.value.delete, null)
     }
   }
 }
