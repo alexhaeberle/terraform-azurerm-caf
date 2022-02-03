c1cba0e4d252852ebf10dfe1da2f0e6c904650cf
Alex Haeberle <alex.haeberle@gmail.com>
2022-02-03 16:51:23 -0500

    finding subnet id is hard

--- networking_private_endpoints.tf
+++ networking_private_endpoints.tf
@@ -8,7 +8,7 @@ module "private_endpoints" {
   name              = each.value.name
   resource_groups   = local.combined_objects_resource_groups
   private_dns       = local.combined_objects_private_dns
-  subnet_id         = try(local.combined_objects_networking[each.value.vnet_key].subnets[each.value.subnet_key].id, null)
+  subnet_id         = try(var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key], try(var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key], each.value.virtual_network_id))
 
   base_tags = try(local.global_settings.inherit_tags, false) ? coalesce(
     try(local.resource_groups[each.value.resource_group_key].tags, null),
