c88bd55e91445b0ca3b84021a0b8211e1e86b599
Alex Haeberle <alex.haeberle@gmail.com>
2022-03-17 15:14:00 -0400

    dyn sec kv

--- modules/security/dynamic_keyvault_secrets/keyvault.tf
+++ modules/security/dynamic_keyvault_secrets/keyvault.tf
@@ -1,37 +1,39 @@
 module "secret" {
   source = "./secret"
-  for_each = {
+  /* for_each = {
     for key, value in var.settings : key => value
     if try(value.value, null) == null
-  }
+  } */
 
-  name  = each.key.secret_name
-  value = can(each.key.output_key) && (can(each.key.resource_key) || can(each.key.attribute_key)) ? lookup(lookup(var.objects[each.key.output_key], try(each.key.resource_key, ""), var.objects[each.key.output_key]), each.key.attribute_key, null) : each.key.value
+  name  = var.settings.secret_name
+  /* value = can(each.key.output_key) && (can(each.key.resource_key) || can(each.key.attribute_key)) ? lookup(lookup(var.objects[each.key.output_key], try(each.key.resource_key, ""), var.objects[each.key.output_key]), each.key.attribute_key, null) : each.key.value */
+  value = var.settings.value
   # for future generations: double lookup because each.value.resource_key is optional
   keyvault_id = var.keyvault.id
 }
 
 module "secret_value" {
   source = "./secret"
-  for_each = {
+  /* for_each = {
     for key, value in var.settings : key => value
     if try(value.value, null) != null && try(value.value, null) != ""
-  }
+  } */
 
-  name        = each.key.secret_name
-  value       = each.key.value
+  name        = var.settings.secret_name
+  value       = var.settings.value
   keyvault_id = var.keyvault.id
 }
 
 module "secret_immutable" {
   source = "./secret_immutable"
-  for_each = {
+  /* for_each = {
     for key, value in var.settings : key => value
     if try(value.value, null) == ""
-  }
+  } */
 
-  name  = each.key.secret_name
-  value = can(each.key.output_key) && (can(each.key.resource_key) || can(each.key.attribute_key)) ? lookup(lookup(var.objects[each.key.output_key], try(each.key.resource_key, ""), var.objects[each.key.output_key]), each.key.attribute_key, null) : each.key.value
+  name  = var.settings.secret_name
+  /* value = can(each.key.output_key) && (can(each.key.resource_key) || can(each.key.attribute_key)) ? lookup(lookup(var.objects[each.key.output_key], try(each.key.resource_key, ""), var.objects[each.key.output_key]), each.key.attribute_key, null) : each.key.value */
+  value = var.settings.value
   # for future generations: double lookup because each.value.resource_key is optional
   keyvault_id = var.keyvault.id
 }
