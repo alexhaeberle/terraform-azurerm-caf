3c8aa6dc80a3c6f31fbaf61179d16b0cb4264414
Jonathan Bull <jonathan.bull@kyndryl.com>
2022-04-18 21:11:55 +0000

    Add access time policies to blob lifecycle management

--- networking_private_endpoints.tf
+++ networking_private_endpoints.tf
@@ -2,13 +2,13 @@ module "private_endpoints" {
   source   = "./modules/networking/private_endpoint/"
   for_each = try(var.networking.private_endpoints, {})
 
-  global_settings   = local.global_settings
-  client_config     = local.client_config
-  settings          = each.value
-  name              = each.value.name
-  resource_groups   = local.combined_objects_resource_groups
-  private_dns       = local.combined_objects_private_dns
-  subnet_id         = try(local.combined_objects_networking[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, try(local.combined_objects_networking[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id, each.value.virtual_network_id))
+  global_settings = local.global_settings
+  client_config   = local.client_config
+  settings        = each.value
+  name            = each.value.name
+  resource_groups = local.combined_objects_resource_groups
+  private_dns     = local.combined_objects_private_dns
+  subnet_id       = try(local.combined_objects_networking[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, try(local.combined_objects_networking[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id, each.value.virtual_network_id))
 
   base_tags = try(local.global_settings.inherit_tags, false) ? coalesce(
     try(local.resource_groups[each.value.resource_group_key].tags, null),
